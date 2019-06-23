83499c9fc0ae37fc75c1669ccc7bbe209e64776d
elorenzen <eric.lorenzen@gmail.com>
2019-06-23 14:09:01 -0600

    Require mongoose and connect to MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express        = require('express');
-const app            = express();
+const mongoose = require('mongoose');
+const express  = require('express');
+const keys     = require('./config/keys');
+const app      = express();
 require('./services/passport');
 
 //Passes app to authRoutes file
 require('./routes/authRoutes')(app);
+
+// Connect mongoose to database
+mongoose.connect(keys.mongoURI);
 
 // If in development mode, set PORT to 5000.
 // Otherwise, set PORT to one assigned by Heroku
