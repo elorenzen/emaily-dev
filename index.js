c6d409153cc0b9d700fc2f7f1eb9d44d0fd0a623
elorenzen <eric.lorenzen@gmail.com>
2019-06-23 10:23:12 -0600

    Create basic passport call to GoogleStrategy

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const app = express();
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport       = require('passport');
+const express        = require('express');
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-})
+const app            = express();
+
+passport.use(new GoogleStrategy());
+
 
 // If in development mode, set PORT to 5000.
 // Otherwise, set PORT to one assigned by Heroku
