829f3a50fc1dcaecf61cf0e471fcad4b32407a57
elorenzen <eric.lorenzen@gmail.com>
2019-06-23 07:45:24 -0600

    Create constant variable to set PORT

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+})
+
+// If in development mode, set PORT to 5000.
+// Otherwise, set PORT to one assigned by Heroku
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
